4bb5996741e03abf2972ea9c668a3d26fb45b5f1
Jordan Dalton <jordan.8474@gmail.com>
2017-03-28 00:25:26 +0100

    Add what I have so far..
    Screenshot taking (saved to "{data}/Captures/{file}" where "{data}" is the dataPath set in unity and "{file}" is "capture_{time}.png" where "{time}" is the current timestamp.
    An ugly hack to get python scripts to run with the Unity game engine

---
+++ Assets/Scripts/TakePhoto.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TakePhoto : MonoBehaviour
+{
+    public string basePath = ""; // by default use Application.dataPath
+    public string pictureFolder = "Captures";
+    public string fileFormat = "capture_{0}.png";
+
+    private void Start()
+    {
+        if (string.IsNullOrEmpty(basePath))
+            basePath = Application.dataPath;
+    }
+
+    public void TakePhotoOnClick()
+    {
+        string baseDir = Path.Combine(basePath, pictureFolder);
+        if (!Directory.Exists(baseDir))
+            Directory.CreateDirectory(baseDir);
+
+        string formatFile = string.Format(fileFormat, "test_file" );
+
+        string filename = Path.Combine(baseDir, formatFile);
+
+        Application.CaptureScreenshot(filename);
+
+        Debug.Log("Saving capture to: " + filename);
+    }
+
+}

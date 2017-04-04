355ca0cbc8693a5a796b5b5c27147aba90fc2534
Jordan Dalton <jordan.8474@gmail.com>
2017-04-04 05:18:50 +0100

    Experimental UI changes
    Just testing to see if we can draw the props directly onto the GUI rather than creating a new object for them

--- Assets/Scripts/Augmented Reality/HatHandler.cs
+++ Assets/Scripts/Augmented Reality/HatHandler.cs
@@ -6,11 +6,11 @@ using Kinect = Windows.Kinect;
 
 public class HatHandler : AbstractClothHandler
 {
-    
+    Vector3 position = new Vector3();
     public override void UpdatePosition(ulong uniqeId, Kinect.Joint joint, Vector3 jointWorldPosition)
     {
         Cloth hat = CurrentCloth;
-        GameObject hatObj = GetOrCreateObject("Hat_" + uniqeId, hat, mainUI);
+        GameObject hatObj = GetOrCreateObject("Hat_" + uniqeId, hat, Camera.main.gameObject);
         /*
         Camera cam = Camera.main;
         float height = 2f * cam.orthographicSize;
@@ -20,6 +20,19 @@ public class HatHandler : AbstractClothHandler
         float newX = jointWorldPosition.x - width;
         float newY = jointWorldPosition.y - height;
         */
-        hatObj.transform.localPosition = new Vector3((jointWorldPosition.x * 100) + (hat.xOffset*100), (jointWorldPosition.y * 100) + (hat.yOffset*100), 10);
+        Camera cam = Camera.main;
+        Vector3 point = cam.ScreenToViewportPoint(jointWorldPosition);
+        //hatObj.transform.localPosition = point;
+        position = jointWorldPosition;
+        //hatObj.transform.localPosition = new Vector3((jointWorldPosition.x * 100) + (hat.xOffset*100), (jointWorldPosition.y * 100) + (hat.yOffset*100), 10);
     }
+
+    public override void DrawObject()
+    {
+        
+        GUI.DrawTexture(new Rect(position.x + CurrentCloth.xOffset, position.y + CurrentCloth.yOffset,
+                                100 * CurrentCloth.scaleX, 100 * CurrentCloth.scaleY),
+            CurrentCloth.image.texture);
+    }
+
 }
